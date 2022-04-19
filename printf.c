2dae6db2215ac886f6ac7431de63143458f9df9b
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-04-19 23:54:07 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,44 +1,51 @@
 #include "main.h"
+
 /**
-* _printf- Function that works like printf
-* @format: string argument passed to variadic function
-*
-* Return: Number of characters passed to the console
-*/
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
+ */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
+
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		int len = 0;
-
-		specifier sp[] = {
-			{"c", print_char},
-			{"s", print_string},
-			{"%", print_percent},
-			{"d", print_integer},
-			{"i", print_integer},
-			{"x", print_hex},
-			{"X", print_HEX},
-			{"o", print_octal},
-			{"b", print_binary},
-			{"u", print_unsigned},
-			{"r", print_rev},
-			{"R", print_rot},
-			{"S", print_the_string},
-			{NULL, NULL},
-		};
-
-		va_list arg;
-
-		va_start(arg, format);
+		if (format[i] == '%')
 		{
-			len += entry_printf(sp, format, arg);
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
-
-		va_end(arg);
-
-		return (len);
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-
-	return (-1);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
